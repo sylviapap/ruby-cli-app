3f0859d984f0fc94318761bc0bb36760729ddc30
Sylvia <sylviapap1@gmail.com>
2020-02-11 18:03:33 -0800

    add evidence

--- app/test.rb
+++ app/test.rb
@@ -4,32 +4,40 @@ require 'json'
 
 app_id = '582e2307'
 app_key = 'c98b58a9bf15795b1dacdfebe5375701'
+
+
 symptoms = RestClient.get("https://api.infermedica.com/v2/symptoms", headers={'App-Id' => app_id, 'App-Key' => app_key})
 
 diseases = RestClient.get("https://api.infermedica.com/v2/conditions", headers={'App-Id' => app_id, 'App-Key' => app_key})
 
 info = RestClient.get("https://api.infermedica.com/v2/info", headers={'App-Id' => app_id, 'App-Key' => app_key})
 
-data_hash = {
-    'sex' => 'female',
-    'age' => 25,
-    'evidence' => [
-      {'id' => 's_47', 'choice_id' => 'present', 'initial' => true},
-      {'id' => 's_22', 'choice_id' => 'present', 'initial' => true},
-      {'id' => 'p_81', 'choice_id' => 'absent'}
-    ]
-}
+risk_factors = RestClient.get("https://api.infermedica.com/v2/risk_factors", headers={'App-Id' => app_id, 'App-Key' => app_key})
 
-payload = JSON.generate(data_hash)
-
-# binding.pry
-
-diagnosis = RestClient.post("https://api.infermedica.com/v2/diagnosis", payload, headers = {'App-Id' => app_id, 'App-Key' => app_key, 'Content-Type' => 'application/json'}
-)
+search = RestClient.get("https://api.infermedica.com/v2/search?phrase=cough", headers={'App-Id' => app_id, 'App-Key' => app_key})
+search_hash = JSON.parse(search)
+new_array = search_hash.map do |hash|
+    hash.delete_if {|key, value| key >= "label"}
+    end
+array_of_hashes = new_array.each {|hash| hash['choice_id'] = 'present'}
 
 symptoms_hash = JSON.parse(symptoms)
 diseases_hash = JSON.parse(diseases)
 info_hash = JSON.parse(info)
+risk_factors_hash = JSON.parse(risk_factors)
+
+
+data_hash = {
+    'sex' => 'female',
+    'age' => 25,
+    'evidence' => array_of_hashes
+}
+
+payload = JSON.generate(data_hash)
+
+diagnosis = RestClient.post("https://api.infermedica.com/v2/diagnosis", payload, headers = {'App-Id' => app_id, 'App-Key' => app_key, 'Content-Type' => 'application/json'})
+
 diagnosis_hash = JSON.parse(diagnosis)
-# binding.pry
+
+binding.pry
 puts 'hi'
