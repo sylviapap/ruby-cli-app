bf67bc195acae45d8a10a6b93d93d7db5bf09712
amnyrrd <amnyrrd@gmail.com>
2020-02-13 15:56:03 -0800

    1 and 2 work

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 # require_relative '../lib/cli.rb'
 
-run_symptom_checker
+# run_symptom_checker
+welcome
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
+app_id = '582e2307'
+app_key = 'c98b58a9bf15795b1dacdfebe5375701'
 
+result = RestClient.get("https://api.infermedica.com/v2/conditions", headers={'App-Id' => app_id, 'App-Key' => app_key})
+disease_array = JSON.parse(result)
+name_array = disease_array.map do |hash|
+    hash.select {|key, value| key >= "name"}
+    end
+name_array.each {|disease| Disease.create(name: disease["name"])}
 
