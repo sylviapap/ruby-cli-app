cc2e99c3c0ed78ca7f7d01f68768f1e8052cdb63
Sylvia <sylviapap1@gmail.com>
2020-02-14 10:53:55 -0800

    patientdisease

--- app/lib/cli.rb
+++ app/lib/cli.rb
@@ -45,7 +45,8 @@ def welcome
     puts "2  -  To view all possible diseases in the database, enter 2."
     puts "3  -  To search from a list of risk factors, enter 3."
     puts "4  -  To return to main menu, enter 4."
-    puts "5  -  To exit Symptom Checker, enter any other key."
+    puts "5  -  To view saved diseases, enter 5."
+    puts "6  -  To exit Symptom Checker, enter any other key."
     response = gets.chomp
     if response == "1"
         run_symptom_checker
@@ -63,6 +64,8 @@ def welcome
         puts format(rf_result)
     elsif response == "4"
         welcome   
+    elsif response == "5"
+        view_patient_diseases
     end
 end
 
@@ -84,23 +87,19 @@ def run_symptom_checker
     get_diagnosis(sex_input, age_input, symptom_input, patient)
     puts ""
     puts "Enter 9 to run Symptom Checker again."
-    puts "Enter 8 to view all of your possible diseases."
+    puts ""
+    puts "Enter 5 to view all of your possible diseases."
+    puts ""
     puts "Enter any other key to exit."
     puts ""
     num_response = gets.chomp
     if num_response == "9"
         run_symptom_checker
     end
-    if num_response == "8"
+    if num_response == "5"
         view_patient_diseases
     end
 end
-
-# def run_again
-#     #might need this to avoid duplicates?
-#     puts "Enter a symptom"
-#     symptom_input = gets.chomp
-# end
 
 def get_symptoms(symptom_input)
     app_id = "582e2307"
@@ -118,7 +117,6 @@ def save_patient(name_input, age_input, sex_input)
 end
 
 def get_diagnosis(sex_input, age_input, symptom_input, patient)
-######added PatientDisease save
     app_id = "582e2307"
     app_key = "c98b58a9bf15795b1dacdfebe5375701"
     new_array = get_symptoms(symptom_input).map do |hash|
@@ -142,11 +140,12 @@ def get_diagnosis(sex_input, age_input, symptom_input, patient)
 end
 
 def view_patient_diseases
-    # returns all diseases after multiple runs of symptom checker
-    patient_disease = PatientDisease.all
-    result = patient_disease.map {|pd| Disease.where(id: pd.disease_id).pluck(:name)}
-    # result = Disease.where(id: PatientDisease.disease_id).pluck(:name)
-    puts result
+    puts format("Enter your name (exactly as you entered before)")
+    name_input = gets.chomp
+    patient = Patient.find_by(name: name_input)
+    patient_diseases = PatientDisease.where(patient_id: patient.id)
+    result = patient_diseases.map {|pd| Disease.where(id: pd.disease_id).pluck(:name)}
+    puts format(result)
 end
 
 # binding.pry
